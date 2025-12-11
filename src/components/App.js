78b7ac63b27d0e37c4687ea853927e17b67f5d90
= <=>
2025-12-11 22:44:07 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <div className="greeting-container">
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={handleChange}
+          className="name-input"
+        />
+        {name && <h2 className="greeting">Hello, {name}!</h2>}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
